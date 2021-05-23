71c5b8a60be2864e9f9cd16874a58e4f056c69d9
DigitalPresales <84257933+DigitalPresales@users.noreply.github.com>
2021-05-23 12:07:27 +0530

    Create Scenario1- python2.py

---
+++ Scenario1- python2.py
@@ -0,0 +1,11 @@
+# This code goes into Ubuntu (Seller Machine). 
+import os, sys
+#You can capture logged in user by using $USER and similarly email and phone details from database in real time scenario. For demo , we are hardcoding it to below details.
+#str = os.system('echo "$USER"')
+#str2 = sys.argv
+file_object = open('<File path of output file here>/o2.csv', 'a')
+file_object.write('Buyer Name- Senthil Balaji')
+file_object.write('Contact details - Email - Senthilb@gmail.com, Phone-408 453 54456')
+file_object.close()
+
+
