46d2a55161d045a07a7c29e767d21bd666dcc70c
DigitalPresales <84257933+DigitalPresales@users.noreply.github.com>
2021-05-23 12:01:32 +0530

    Create Scenario1- Program2.sh

---
+++ Scenario1- Program2.sh
@@ -0,0 +1,5 @@
+python3 python2.py   
+python3 python22.py 
+ 
+cd /opt/kafka/bin/
+sh kafka-console-producer.sh --topic logstopic < <File path of your output file which is>/o2.csv --bootstrap-server localhost:9092
